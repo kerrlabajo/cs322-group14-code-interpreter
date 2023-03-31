462d652e0a1619844470b093ef3a2ed9fcb18843
Brent Lasaca <brent.lasaca@cit.edu>
2023-03-31 22:55:48 +0800

    Check BEGIN CODE and END CODE

--- Interpreter/Grammar/CodeVisitor.cs
+++ Interpreter/Grammar/CodeVisitor.cs
@@ -11,6 +11,29 @@ namespace Interpreter.Grammar
 {
     public class CodeVisitor : CodeGrammarBaseVisitor<object?>
     {
+        public Dictionary<string, object?> _variables { get; } = new Dictionary<string, object?>();
 
+        public override object? VisitProgram([NotNull] CodeGrammarParser.ProgramContext context)
+        {
+            string program = context.GetText().Trim();
+            if (program.StartsWith("BEGIN CODE") && program.EndsWith("END CODE"))
+            {
+                Console.WriteLine("Code contains BEGIN CODE and END CODE");
+            }
+            else if (program.StartsWith("BEGIN CODE") && program.EndsWith("BEGIN CODE"))
+            {
+                Console.WriteLine("Code is missing END CODE");
+            }
+
+            else if (program.EndsWith("END CODE"))
+            {
+                Console.WriteLine("Code is missing BEGIN CODE");
+            }
+            else
+            {
+                Console.WriteLine("Code does not contain BEGIN CODE and END CODE");
+            }
+            return null;
+        }
     }
 }
