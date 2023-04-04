7472dd5a9aaed7d7316d04a1f3099f11323f0f2f
Jie Ann Leonor <jieann.leonor@cit.edu>
2023-04-04 13:57:32 +0800

    Implement high precedence operator

--- Interpreter/Grammar/CodeVisitor.cs
+++ Interpreter/Grammar/CodeVisitor.cs
@@ -276,5 +276,95 @@ namespace Interpreter.Grammar
             return null;
         }
 
+        public override object? VisitMultDivModExpression([NotNull] CodeGrammarParser.MultDivModExpressionContext context)
+        {
+            if (context.children.Count == 1)
+            {
+                // Return the single operator character as a string
+                return context.GetText();
+            }
+            else
+            {
+                // There are two child nodes, so we need to handle the operator and the operands
+                var left = Visit(context.expression(0));
+                var right = Visit(context.expression(1));
+
+                var op = context.highPrecedenceOperator().GetText();
+
+                // Check the types of the operands
+                if (left is int leftInt && right is int rightInt)
+                {
+                    // Both operands are integers
+                    switch (op)
+                    {
+                        case "*":
+                            return leftInt * rightInt;
+                        case "/":
+                            return leftInt / rightInt;
+                        case "%":
+                            return leftInt % rightInt;
+                        default:
+                            throw new ArgumentException($"Unknown operator: {op}");
+                    }
+                }
+                else if (left is float leftFloat && right is float rightFloat)
+                {
+                    // Both operands are doubles
+                    switch (op)
+                    {
+                        case "*":
+                            return leftFloat * rightFloat;
+                        case "/":
+                            return leftFloat / rightFloat;
+                        case "%":
+                            throw new ArgumentException($"Operator '%' cannot be applied to operands of type 'float'");
+                        default:
+                            throw new ArgumentException($"Unknown operator: {op}");
+                    }
+                }
+                else if (left is int leftInt2 && right is float rightFloat2)
+                {
+                    // One operand is an integer and the other is a double
+                    switch (op)
+                    {
+                        case "*":
+                            return leftInt2 * rightFloat2;
+                        case "/":
+                            return leftInt2 / rightFloat2;
+                        case "%":
+                            throw new ArgumentException($"Operator '%' cannot be applied to operands of type 'int' and 'float'");
+                        default:
+                            throw new ArgumentException($"Unknown operator: {op}");
+                    }
+                }
+                else if (left is float leftFloat2 && right is int rightInt2)
+                {
+                    // One operand is a double and the other is an integer
+                    switch (op)
+                    {
+                        case "*":
+                            return leftFloat2 * rightInt2;
+                        case "/":
+                            return leftFloat2 / rightInt2;
+                        case "%":
+                            throw new ArgumentException($"Operator '%' cannot be applied to operands of type 'float' and 'int'");
+                        default:
+                            throw new ArgumentException($"Unknown operator: {op}");
+                    }
+                }
+                else if (left == null || right == null)
+                {
+                    throw new ArgumentNullException("Operand cannot be null.");
+                }
+                else
+                {
+                    // Operands are of different types
+                    throw new ArgumentException($"Cannot perform operation on operands of different types: {left.GetType().Name} and {right.GetType().Name}");
+                }
+            }
+        }
+
+        
+
     }
 }
